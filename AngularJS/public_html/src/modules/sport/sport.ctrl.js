18d331d9b8191e04749a7acfb8f7d20fc68ebf60
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-02-23 16:27:13 -0500

    Tabs to spaces
    Se cambian las tabulaciones por espacios

--- AngularJS/public_html/src/modules/sport/sport.ctrl.js
+++ AngularJS/public_html/src/modules/sport/sport.ctrl.js
@@ -1,9 +1,9 @@
 (function () {
-	var app = angular.module('sportModule');
+    var app = angular.module('sportModule');
 
-	app.controller('sportCtrl', ['$scope', 'CRUDUtils', 'sport.context', function ($scope, CRUDUtils, context) {
-			this.url = context;
-			CRUDUtils.extendCtrl(this, $scope);
-			this.fetchRecords();
-		}]);
+    app.controller('sportCtrl', ['$scope', 'CRUDUtils', 'sport.context', function ($scope, CRUDUtils, context) {
+            this.url = context;
+            CRUDUtils.extendCtrl(this, $scope);
+            this.fetchRecords();
+        }]);
 })();
--- AngularJS/public_html/src/shared/mocks/mock.svc.js
+++ AngularJS/public_html/src/shared/mocks/mock.svc.js
@@ -1,15 +1,15 @@
 (function () {
-	var mock = angular.module('MockModule');
-	
-	mock.value('MockModule.mockRecords', {});
+    var mock = angular.module('MockModule');
 
-	mock.provider('MockModule.urls', function () {
-		var context = [];
-		this.registerUrl = function (value) {
-			context.push(value);
-		};
-		this.$get = function () {
-			return context;
-		};
-	});
+    mock.value('MockModule.mockRecords', {});
+
+    mock.provider('MockModule.urls', function () {
+        var context = [];
+        this.registerUrl = function (value) {
+            context.push(value);
+        };
+        this.$get = function () {
+            return context;
+        };
+    });
 })();
