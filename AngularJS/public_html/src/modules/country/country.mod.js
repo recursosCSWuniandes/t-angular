3df0f9b040ce87c8629531d261024a123d9afdc2
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-02-25 08:20:02 -0500

    Corrección mocks country
    Se mueve código de implementación de mocks a un nuevo archivo.

--- AngularJS/public_html/src/modules/country/country.mod.js
+++ AngularJS/public_html/src/modules/country/country.mod.js
@@ -6,21 +6,4 @@
     countryModule.config(['country.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
             urlsProvider.registerUrl(context);
         }]);
-
-    countryModule.run(['$httpBackend', 'country.context', 'MockModule.mockRecords', function ($httpBackend, context, mockRecords) {
-            $httpBackend.whenGET('webresources/' + context + '/mostPopulated').respond(function () {
-                var top;
-                var collection = mockRecords[context];
-                for (var i in collection) {
-                    if (!top && collection[i].population) {
-                        top = collection[i];
-                    } else {
-                        if (collection[i].population && top.population < collection[i].population) {
-                            top = collection[i];
-                        }
-                    }
-                }
-                return [200, top || {}, {}];
-            });
-        }]);
 })();
